c823474a6d344392dd4ebe13ef07dae8ab8bcbad
beckerashton <117123540+beckerashton@users.noreply.github.com>
2023-04-10 22:11:39 -0400

    css style sheet setup

--- neko/index.js
+++ neko/index.js
@@ -3,7 +3,7 @@ import  { createRoot }  from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './src/App.js'
 
-
+import './src/styles/base.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
